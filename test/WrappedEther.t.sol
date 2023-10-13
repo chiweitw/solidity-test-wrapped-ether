27f3e19a606ea58f1379e85cd090d7d90145dc44
chiwei.huang <chiwei.huang@chiweihuangdeMacBook-Air.local>
2023-10-13 16:46:04 +0800

    add testDepositETHtoContract

--- test/WrappedEther.t.sol
+++ test/WrappedEther.t.sol
@@ -8,22 +8,32 @@ contract WrappedEtherTest is Test {
     WrappedEther public weth;
     address user1;
     address user2;
+    uint256 msgValue;
 
     function setUp() public {
         weth = new WrappedEther();
         user1 = makeAddr("Alice");
         user2 = makeAddr("Bob");
+        msgValue = 1 ether;
+        vm.startPrank(user1); // Set msg.sender for subsequent calls
+        deal(user1, msgValue);  // set enough ether balance for user1 
     }
 
     // 測項 1: deposit 應該將與 msg.value 相等的 ERC20 token mint 給 user
     function testDeposit() public {
-        uint amount = 1 ether; // declare desposit amount
-        deal(user1, amount);  // set ether balance for user1 
-        vm.startPrank(user1); // Set msg.sender for subsequent calls
-        weth.deposit{value: amount}();
-        uint256 wethBalance = weth.balanceOf(user1);
+        weth.deposit{value: msgValue}();
+        uint256 user1WethBalance = weth.balanceOf(user1);
 
-        assertEq(wethBalance, amount); // weth balance of user1 should equal to deposit amount 
+        assertEq(user1WethBalance, msgValue);
+        vm.stopPrank();
+    }
+
+    // 測項 2: deposit 應該將 msg.value 的 ether 轉入合約
+    function testDepositETHtoContract() public {
+        weth.deposit{value: msgValue}();
+        uint256 wethEthBalance = address(weth).balance;
+
+        assertEq(wethEthBalance, msgValue); // wethETHBalance from 0 to msgValue
         vm.stopPrank();
     }
 }
